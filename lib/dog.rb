5f45f1f8197f4113d63fa4296eb740c4f423d803
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-12-16 02:26:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,12 +14,12 @@ class Dog
       breed TEXT
     )
     SQL
-    DB[:conn].excute(sql)
+    DB[:conn].execute(sql)
   end
   def self.drop_table
     sql = <<-SQL
     DROP TABLE IF EXISTS dogs
     SQL
-    DB[:conn].excute(sql)
+    DB[:conn].execute(sql)
   end
 end
