7ed082b084bcc2cadc166f5af1050ec0cc07c094
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-12-16 02:30:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -22,4 +22,18 @@ class Dog
     SQL
     DB[:conn].execute(sql)
   end
+  def save
+  end
+  def self.create
+  end
+  def self.new_from_db
+  end
+  def self.find_by_id
+  end
+  def self.find_or_create_by
+  end
+  def self.find_by_name
+  end
+  def update
+  end
 end
